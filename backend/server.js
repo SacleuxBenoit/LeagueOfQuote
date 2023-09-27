9c89bbabc7b81c3087ce7963f3e01ffb0f785d46
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-09-27 15:11:23 +0200

    add basic express server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+const express = require('express')
+const app = express()
+const env = require('dotenv').config()
+const port = process.env.PORT
+
+app.listen(port, () => {
+    console.log(`PORT IS LISTENING AT PORT : ${port}`)
+})
