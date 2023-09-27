986189bdc8c69f6e38fd67a8b3d8a5d8b5047cf9
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-09-27 15:37:53 +0200

    install express-async-handler

--- backend/controllers/quoteControllers.js
+++ backend/controllers/quoteControllers.js
@@ -1,22 +1,25 @@
-const getQuotes = (req,res) => {
+const asyncHandler = require('express-async-handler')
+
+const getQuotes = asyncHandler(async (req,res) => {
     if(!req.body.quote || !req.body.author){
         res.status(400)
         throw new Error('Please add all fields')
     }
     res.status(200).json({message: 'get quotes'})
-}
+})
 
-const createQuotes = (req,res) => {
+const createQuotes = asyncHandler(async (req,res) => {
     res.status(200).json({message: 'Create quotes'})
-}
+})
 
-const updateQuotes = (req,res) => {
+const updateQuotes = asyncHandler(async(req,res) => {
     res.status(200).json({message: 'Update quotes'})
-}
+})
 
-const deleteQuotes = (req,res) => {
+const deleteQuotes = asyncHandler(async (req,res) => {
     res.status(200).json({message: 'Delete quotes'})
-}
+})
+
 module.exports = {
     getQuotes,
     createQuotes,
