86e412770a15b2b4bd5f5e78d06033bb302885fd
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-09-28 14:27:40 +0200

    add connection to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const app = express()
 const env = require('dotenv').config()
 const port = process.env.PORT
+
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
